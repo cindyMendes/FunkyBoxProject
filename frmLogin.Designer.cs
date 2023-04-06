15e9b8ecffaabbc8fc4e8f549935abe9d84ae8ca
Nico <Nicaoss@users.noreply.github.com>
2023-04-06 14:47:03 +0200

    frmLogin

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace FunkyBox
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.log = new System.Windows.Forms.Label();
+            this.mdp = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.valid = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // log
+            // 
+            this.log.AutoSize = true;
+            this.log.Location = new System.Drawing.Point(41, 37);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(33, 13);
+            this.log.TabIndex = 0;
+            this.log.Text = "Login";
+            this.log.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // mdp
+            // 
+            this.mdp.AutoSize = true;
+            this.mdp.Location = new System.Drawing.Point(41, 93);
+            this.mdp.Name = "mdp";
+            this.mdp.Size = new System.Drawing.Size(71, 13);
+            this.mdp.TabIndex = 1;
+            this.mdp.Text = "Mot de passe";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // valid
+            // 
+            this.valid.Location = new System.Drawing.Point(44, 176);
+            this.valid.Name = "valid";
+            this.valid.Size = new System.Drawing.Size(75, 23);
+            this.valid.TabIndex = 4;
+            this.valid.Text = "Valider";
+            this.valid.UseVisualStyleBackColor = true;
+            this.valid.Click += new System.EventHandler(this.valid_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(147, 176);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(75, 23);
+            this.Quit.TabIndex = 5;
+            this.Quit.Text = "Quitter";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(147, 112);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(125, 17);
+            this.checkBox.TabIndex = 6;
+            this.checkBox.Text = "Afficher les caratères";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(65, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mot de passe incorrect. Désolé.";
+            this.label3.Visible = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.valid);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mdp);
+            this.Controls.Add(this.log);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label log;
+        private System.Windows.Forms.Label mdp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button valid;
+        private System.Windows.Forms.Button Quit;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ frmLogin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunkyBox
+{
+    public partial class frmLogin : Form
+    {
+        OleDbConnection connexion = new OleDbConnection();
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void valid_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Veuillez remplir tout les champs nécessaire.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (textBox1.Text == "login")
+            {
+                if (textBox2.Text == "mdp")
+                    this.Hide();
+            }
+            else
+                label3.Visible = true;
+
+           
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            var resultat = MessageBox.Show("Etes-vous sûr de vouloir quitter ?","Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (resultat == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox.Checked)
+            
+                textBox2.UseSystemPasswordChar = false;
+             
+            else
+                textBox2.UseSystemPasswordChar = true;
+        
+        }
+    }
+}
