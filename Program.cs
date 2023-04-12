67a756ec3afa69129f7fb1b7e90837ab3b00c7f5
Nicaoss <118338695+Nicaoss@users.noreply.github.com>
2023-04-12 09:55:50 +0200

    affichage client 3

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace FunkyBox
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDisplayGarages());
+            Application.Run(new frmDisplayCustomers());
         }
     }
 }
