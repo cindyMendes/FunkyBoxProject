d9bf28a768632adc89b0b9f5f004d5abcaab03f1
LaurentAFPA <laurentcampan@outlook.fr>
2023-04-12 10:04:19 +0200

    Create Garage Display

--- frmCreateGarage.cs
+++ frmCreateGarage.cs
@@ -12,21 +12,22 @@ namespace FunkyBox
 {
     public partial class frmCreateGarage : Form
     {
-        DataTable displayGarages;
+        public static DataTable displayGarages = new DataTable();
         public frmCreateGarage()
         {
             InitializeComponent();
         }
         public void AddGarage()
         {
+            Garage twoGarage = new Garage(txtGarageNb.Text, txtParkingName.Text, Double.Parse(txtMonthlyRent.Text), Double.Parse(txtMonthlyFees.Text), txtTenantName.Text, DateTime.Parse(txtStartRent.Text));
             DataRow dr;
             dr = displayGarages.NewRow();
-            dr[0] = textGarageNb.Text;
-            dr[1] = txtParkingName.Text;
-            dr[2] = txtMonthlyRent.Text;
-            dr[3] = txtMonthlyFees.Text;
-            dr[4] = txtTenantName.Text;
-            dr[5] = txtStartRent.Text;
+            dr[0] = twoGarage.GarageId;
+            dr[1] = twoGarage.ParkingName;
+            dr[2] = twoGarage.MonthlyRent;
+            dr[3] = twoGarage.MonthlyFees;
+            dr[4] = twoGarage.TenantName;
+            dr[5] = twoGarage.StartRent;
             displayGarages.Rows.Add(dr);
         }
 
@@ -40,8 +41,8 @@ namespace FunkyBox
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            this.Hide();
             AddGarage();
+            this.Hide();
             frmDisplayGarages frmDG = new frmDisplayGarages();
             frmDG.ShowDialog();
             this.Close();
--- frmDisplayGarages.cs
+++ frmDisplayGarages.cs
@@ -12,6 +12,7 @@ namespace FunkyBox
 {
     internal partial class frmDisplayGarages : Form
     {
+<<<<<<< Updated upstream
         //DataTable displayGarages;
 
         //public frmCreateGarage()
@@ -37,12 +38,46 @@ namespace FunkyBox
         //    dr[5] = txtStartRent.Text;
         //    displayGarages.Rows.Add(dr);
         //}
+=======
+        public Garage oneGarage = new Garage("jkgyf", "Nice Centre", 150, 15, "Dupont", new DateTime(2017, 8, 24));
+        public DataTable displayGarages = frmCreateGarage.displayGarages;
+        
+
+        public frmDisplayGarages()
+        {
+            InitializeComponent();
+
+            if (displayGarages.Columns.Count == 0)
+            {
+                displayGarages.Columns.Add(new DataColumn("Identifiant du box", typeof(System.String)));
+                displayGarages.Columns.Add(new DataColumn("Nom du parking", typeof(System.String)));
+                displayGarages.Columns.Add(new DataColumn("Loyer mensuel", typeof(System.Double)));
+                displayGarages.Columns.Add(new DataColumn("Charges mensuelles", typeof(System.Double)));
+                displayGarages.Columns.Add(new DataColumn("Nom du locataire", typeof(System.String)));
+                displayGarages.Columns.Add(new DataColumn("Début de location", typeof(System.DateTime)));
+            }
+            
+            DataRow dr;
+            dr = displayGarages.NewRow();
+            dr[0] = oneGarage.GarageId;
+            dr[1] = oneGarage.ParkingName;
+            dr[2] = oneGarage.MonthlyRent;
+            dr[3] = oneGarage.MonthlyFees;
+            dr[4] = oneGarage.TenantName;
+            dr[5] = oneGarage.StartRent;
+            displayGarages.Rows.Add(dr);
+            dgvDisplayGarages.DataSource = displayGarages;
+            
+        }
+>>>>>>> Stashed changes
 
         private void btnAddGarage_Click(object sender, EventArgs e)
         {
+            displayGarages.Clear();
             this.Hide(); 
             frmCreateGarage frmCG = new frmCreateGarage();
             frmCG.ShowDialog();
+            
         }
     }
 }
